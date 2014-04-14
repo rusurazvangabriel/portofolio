c84c5986656686637473ed4b75899035a8c7e4c7
Razvin <razvin@razvin-HP-Pavilion-g6-Notebook-PC.(none)>
2014-04-14 15:04:27 +0300

    First Commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,11 @@
+class StaticPagesController < ApplicationController
+  def home #home.html.erb
+
+  end
+
+  def about
+  end
+
+  def contact
+  end	
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Portofoliu::Application.config.secret_key_base = 'fbd3adc61128d0e3b414e8ace1c7bab696790fe60421f7e32717137ec5efec50580cc14ae59e4d6755e8ed663520ba99294d435c858f4430009c2eaf999134a5'
